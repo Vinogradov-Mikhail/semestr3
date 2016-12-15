6705cf2016f89e8932e2347a8e0c6a3172b13626
Vinogradov-Mikhail <Vinogradov97m@gmail.com>
2016-12-15 21:42:25 +0300

    add

---
+++ workThree/workTgree.Test/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace workThree.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private Graph graph;
+        private bool[,] vertexMatrix;
+        private bool[] roborsPosition;
+        private Robots robots;
+
+        [TestMethod]
+        public void Test1()
+        {
+            vertexMatrix = new bool[3, 3] { { true, true, false }, { true, true, true }, { false, true, true } };
+            roborsPosition = new bool[3] { true, false, true };
+            graph = new Graph(vertexMatrix, roborsPosition.Length);
+            robots = new Robots(graph, roborsPosition);
+            Assert.AreEqual(true,robots.RobotsWarWithKillingAll());
+        }
+    }
+}
---
+++ workThree/workThree/Robots.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace workThree
+{
+    public class Robots
+    {
+        private Graph graph;
+        private int countVertex;
+        private bool[] WithRobots;
+
+        public Robots(Graph graph, bool[] positionAdd)
+        {
+            this.graph = graph;
+            this.countVertex = graph.ReturnSize();
+            this.WithRobots = positionAdd;
+        }
+
+        /// <summary>
+        /// checking can robot be destroyed
+        /// </summary>
+        private bool CanDestroy(int thisRobot, bool[] robotIsDestroyed)
+        {
+            Stack<int> StackOfVertex = new Stack<int>();
+            StackOfVertex.Push(thisRobot);
+            bool[] visited = new bool[countVertex];
+            visited[thisRobot] = true;
+            while (StackOfVertex.Count != 0)
+            {
+                int thisVertex = StackOfVertex.Pop();
+                if (thisVertex != thisRobot && WithRobots[thisVertex])
+                {
+                    robotIsDestroyed[thisRobot] = true;
+                    robotIsDestroyed[thisVertex] = true;
+                    return true;
+                }
+                for (int i = 0; i < countVertex; ++i)
+                {
+                    for (int j = 0; j < countVertex && i != thisVertex && graph.HasWay(i, thisVertex); ++j)
+                    {
+                        if (graph.HasWay(i, j) && i != j && !visited[j])
+                        {
+                            visited[j] = true;
+                            StackOfVertex.Push(j);
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// return can all robots will destroyed
+        /// </summary>
+        public bool RobotsWarWithKillingAll()
+        {
+            bool[] isRobotDestroyed = new bool[countVertex];
+
+            for (int i = 0; i < countVertex; ++i)
+            {
+                if (WithRobots[i] && !isRobotDestroyed[i])
+                {
+                    if (!CanDestroy(i, isRobotDestroyed))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
